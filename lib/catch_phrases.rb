87634b39f4d468f998bad04c5a2191d3dc81cc53
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-20 16:39:24 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,6 +9,9 @@ def toadstool
  status = "Thank You Mario! But Our Princess Is In Another Castle!" 
   puts status
   end
+
+def link 
+  
   
   def all_phrases 
     link = "It's Dangerous To Go Alone! Take This."
